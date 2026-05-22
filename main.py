888cb755c6e31010bf984a81cc4bcbcb25ff1956
Jaya741691 <jayasimha1322@gmail.com>
2026-05-22 11:10:38 +0530

    feat:2nd upload1

---
+++ main.py
@@ -0,0 +1,32 @@
+# def fun(*a):
+#     return sum(a)
+# n=fun(1,2,3,4,5,6,7,8)
+# if (n%2)==0:
+#     print(f"{n} is even")
+# else:
+#     print(f"{n} is odd")
+
+# def fun4():
+#     print("s")
+# print(fun4())
+# k=fun4()
+# print(k)
+
+
+# formating string
+
+#
+# a=10
+# k=f"a: {a}"
+# n=f"a: {a+a}"
+# print(k)
+
+
+
+
+
+
+
+
+
+
