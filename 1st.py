11a6c1a3e279fed89a46ef516170c72022cc53ca
Jaya741691 <jayasimha1322@gmail.com>
2026-05-27 11:15:32 +0530

    feat:2nd upload1

---
+++ 1st.py
@@ -0,0 +1,26 @@
+
+#               MAP , FILTER , REDUCE
+
+
+# k=list(filter(lambda x:(x**3)%4==0,map(lambda x:x**3,[1,2,3,4,5])))
+# print(k)
+# from functools import reduce
+#
+# from functools import reduce
+# st=['.','j','o','i','n','(',')']
+# k=reduce(lambda x,y:x+y,st,'@')
+# print(k)
+
+# l=[1,2,3,4,5,6,7]
+# k=reduce(lambda x,y:x if x>y else y,l)
+# print(k)
+
+
+# from functools import reduce
+# c=[0,22,31,35,23]
+# k=list(map(lambda x:((9//5)*x)+32,c))
+# r=list(filter(lambda x:x%3==0,k))
+# m=reduce(lambda x,y:x+y,filter(lambda x:x%3==0,map(lambda x:((9/5)*x)+32,c)),0)
+# print(m)
+
+
