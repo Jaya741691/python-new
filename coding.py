6f24aa31ae4ed0242d199089752275e9e5a1334f
Jaya741691 <jayasimha1322@gmail.com>
2026-05-27 12:43:41 +0530

    feat:coding questions

---
+++ coding.py
@@ -0,0 +1,111 @@
+#                          PRACTICE
+#             1ST
+
+
+# a = float(input())
+# g = a*1000
+# print(a,"kg = ",g,"gram")
+
+
+#               2ND
+
+
+# a = float(input())
+# b = (a*(9/5))+32
+# print(a,"C = ",b,"F")
+
+
+#               3rd
+
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+#
+# if (a>b) and (a>c):
+#     print(a,"is big")
+#
+# if (b>c) and (b>c):
+#     print(b,"is big")
+#
+# else:
+#     print(c,"is big")
+
+
+
+#              4th
+
+
+
+#              5th
+
+
+
+
+#              6th
+
+
+
+# a = int(input('1st : '))
+# b = int(input('2nd : '))
+# c = input('operation : ')
+#
+# if (c=='+') :
+#     print(a+b)
+# elif (c=='*'):
+#     print(a*b)
+# elif (c=='*'):
+#     print(a*b)
+# elif (c=='/'):
+#     print(a/b)
+# elif (c=='//'):
+#     print(a//b)
+# elif (c=='**'):
+#     print(a**b)
+# else:
+#     print("operation is invalid")
+
+
+
+#              7th
+
+# n=int(input())
+# for i in range(n+1):
+#     print('CVCOP')
+
+
+
+#               8th
+
+
+# for i in range(250,551):
+#     if (i%11==0):
+#         print(i)
+
+
+
+#                9th
+
+
+# s=0
+# for i in range(56,153):
+#     s+=i
+# print(s)
+
+
+
+#                10th
+
+
+
+# for i in range(700,901):
+#     if(i%2==0):
+#         print(i)
+
+
+
+
+
+
+
+
