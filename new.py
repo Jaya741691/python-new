269c4e4e6ef13097b940a1b092525cc1951090fc
Jaya741691 <jayasimha1322@gmail.com>
2026-06-10 22:21:58 +0530

    feat:practice

--- new.py
+++ new.py
@@ -1,9 +1,2 @@
-from functools import reduce
-l=["Hello","Hii","who","are","you?"]
-v="aeiouAEIOU"
-k=list(map(lambda x:"".join(list(map(lambda y:y if y not in v else "",x))),l))
-j=list(map(lambda x:"".join(filter(lambda y:y not in v,x)),l))
-m=list(map(lambda x: reduce(lambda y,z: y + ord(z),x,0),j))
-print(k)
-print(j)
-print(m)
+
+    
