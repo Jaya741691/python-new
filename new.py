c11923122768e8fbe135b6f1600565373cb03c3a
Jaya741691 <jayasimha1322@gmail.com>
2026-06-13 23:07:40 +0530

    feat:decorators

--- new.py
+++ new.py
@@ -1,2 +1,15 @@
+def Dec(func):
+    print("Function Called")
+    def Inner():
+        print("Starting Function")
+        print(f"Func : {func.__name__}")
+        func()
+        print("Ending Function")
+    return Inner
 
-    
+@Dec
+def greet():
+    print("hello Bro")
+
+greet()
+print(f"greet : {greet.__name__}")
